89591d430a3c3b724f8b6e59dc3a709e3bbb059b
FishC_Service <fishc_service@126.com>
2024-05-11 16:27:53 +0800

    Project

--- Project/DES.py
+++ Project/DES.py
@@ -26,6 +26,24 @@ class DES:
 
         return hexadecimal_number
     
+    # 二进制数组转十进制整数
+    def bin2dec(self,binary_list):
+        dec = 0
+        for bin in binary_list:
+            dec *= 2
+            dec += bin
+        return dec
+    
+    # 十进制数转为二进制列表
+    def int2bin(self,a, n):
+        assert 0 <= n and a < 2**n
+        res = [0] * n
+
+        for x in range(n):
+            res[n - x - 1] = a % 2
+            a = a // 2
+        return res
+    
      # 异或操作(按数组中的每一位相对应异或)
     def xor(self,a,b):
         return [x^y for x, y in zip(a, b)]
@@ -54,8 +72,52 @@ class DES:
             33, 1, 41, 9, 49, 17, 57, 25]
         return [input[i-1] for i in fp]
 
+    
+    
+    # 循环左移offset位
+    def leftRotate(self,a, offset):
+        return a[offset:] + a[:offset]
+    
+    # PC2置换
+    def PC2(self,key):
+        pc2 = [14, 17, 11, 24, 1, 5, 
+                3, 28, 15, 6, 21, 10,
+                23, 19, 12, 4, 26, 8,
+                16, 7, 27, 20, 13, 2,
+                41, 52, 31, 37, 47, 55,
+                30, 40, 51, 45, 33, 48,
+                44, 49, 39, 56, 34, 53,
+                46, 42, 50, 36, 29, 32]
+        return [key[i-1] for i in pc2]
+    
+    # 根据密钥生成子密钥
+    def generate_subkey(self):
+         # PC1置换
+        pc1_L = [57, 49, 41, 33, 25, 17, 9, 
+            1, 58, 50, 42, 34, 26, 18, 
+            10, 2, 59, 51, 43, 35, 27, 
+            19, 11, 3, 60, 52, 44, 36]
+        pc1_R = [63, 55, 47, 39, 31, 23, 15, 
+            7, 62, 54, 46, 38, 30, 22, 
+            14, 6, 61, 53, 45, 37, 29, 
+            21, 13, 5, 28, 20, 12, 4]
+        
+        left = [self.key[i-1] for i in pc1_L]
+        right = [self.key[i-1] for i in pc1_R]
+        
+        offset = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+        res = []
+
+        for x in range(16):
+            # LeftRotate
+            left = self.leftRotate(left, offset[x])
+            right = self.leftRotate(right, offset[x])
+            # PC2置换产生子钥匙
+            res.append(self.PC2(left + right))
+        return res
+    
     # 扩张置换，将32位的数据扩展到48位
-    def Expand(input):
+    def Expand(self,input):
         e = [32, 1, 2, 3, 4, 5,
         4, 5, 6, 7, 8, 9,
         8, 9, 10, 11, 12, 13,
@@ -66,93 +128,7 @@ class DES:
         28, 29, 30, 31, 32, 1]
         return [input[i-1] for i in e]
         
-    def F(self,RightHalf,subkey):
-        t = self.xor(self.Expand(RightHalf), subkey)
-        t = self.S(t)
-        t = self.P(t)
-        return t
-    
-    # 循环左移offset位
-    def leftRotate(self,a, offset):
-        return a[offset:] + a[:offset]
-    
-    def PC2(self,key):
-        pc2 = [14, 17, 11, 24, 1, 5, 
-                3, 28, 15, 6, 21, 10,
-                23, 19, 12, 4, 26, 8,
-                16, 7, 27, 20, 13, 2,
-                41, 52, 31, 37, 47, 55,
-                30, 40, 51, 45, 33, 48,
-                44, 49, 39, 56, 34, 53,
-                46, 42, 50, 36, 29, 32]
-        return [key[x-1] for x in pc2]
-    
-    # 根据密钥生成子密钥
-    def generate_subkey(self):
-        pc1_l = [57, 49, 41, 33, 25, 17, 9, 
-            1, 58, 50, 42, 34, 26, 18, 
-            10, 2, 59, 51, 43, 35, 27, 
-            19, 11, 3, 60, 52, 44, 36]
-        pc1_r = [63, 55, 47, 39, 31, 23, 15, 
-            7, 62, 54, 46, 38, 30, 22, 
-            14, 6, 61, 53, 45, 37, 29, 
-            21, 13, 5, 28, 20, 12, 4]
-        
-        left = [self.key[i-1] for i in pc1_l]
-        right = [self.key[i-1] for i in pc1_r]
-        
-        offset = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
-        res = []
 
-        for x in range(16):
-            left = self.leftRotate(left, offset[x])
-            right = self.leftRotate(right, offset[x])
-            res.append(self.PC2(left + right))
-        return res
-    
-    # 轮转
-    def round(self,LeftHalf,RightHalf,subkeys): 
-        for i in range(0,16):
-            tmp = [x for x in RightHalf]
-            F_result = self.F(RightHalf,subkeys[i])
-            RightHalf = self.xor(LeftHalf,F_result)
-            LeftHalf = tmp
-
-        return  RightHalf + LeftHalf # 注意最后结果左右顺序调换
-         
-    #加密
-    def encrypt(self,input):
-        input = self.hex2bin(input)
-        ip = self.initial_permutations(input)
-        LeftHalf = ip[0:32]
-        RightHalf = ip[32:]
-        output = self.round(LeftHalf,RightHalf,self.subkeys)
-        binary = self.final_permutations(output)
-        return self.bin2hex(binary)
-    
-    # 解密
-    def decrypt(self,input):
-        input = self.hex2bin(input)
-        ip = self.initial_permutations(input)
-        LeftHalf = ip[0:32]
-        RightHalf = ip[32:]
-        output = self.round(LeftHalf,RightHalf,self.subkeys[::-1])
-        binary = self.final_permutations(output)
-        return self.bin2hex(binary)
-    
-   
-    
-    # 扩张置换，将32位的半块扩展到48位
-    def Expand(self,a):
-        e = [32, 1, 2, 3, 4, 5,
-            4, 5, 6, 7, 8, 9,
-            8, 9, 10, 11, 12, 13,
-            12, 13, 14, 15, 16, 17,
-            16, 17, 18, 19, 20, 21,
-            20, 21, 22, 23, 24, 25,
-            24, 25, 26, 27, 28, 29,
-            28, 29, 30, 31, 32, 1]
-        return [a[x-1] for x in e]
     
     # S盒变换，输入48位，输出32位
     def S(self,a):
@@ -205,25 +181,6 @@ class DES:
 
         return res
     
-    # 二进制数组转十进制整数
-    def bin2dec(self,binary_list):
-        dec = 0
-        for bin in binary_list:
-            dec *= 2
-            dec += bin
-        return dec
-    
-
-    def int2bin(self,a, n):
-        assert 0 <= n and a < 2**n
-        res = [0] * n
-
-        for x in range(n):
-            res[n - x - 1] = a % 2
-            a = a // 2
-        return res
-
-    
     # P置换
     def P(self,a):
         p = [16, 7, 20, 21,
@@ -236,28 +193,61 @@ class DES:
             22, 11, 4, 25]
         return [a[x-1] for x in p]
     
+    # Feistel函数
+    def F(self,RightHalf,subkey):
+        t = self.xor(self.Expand(RightHalf), subkey)
+        t = self.S(t)
+        t = self.P(t)
+        return t
+    
+    # 轮转
+    def round(self,LeftHalf,RightHalf,subkeys): 
+        for i in range(0,16):
+            tmp = [x for x in RightHalf]
+            F_result = self.F(RightHalf,subkeys[i])
+            RightHalf = self.xor(LeftHalf,F_result)
+            LeftHalf = tmp
+        return  RightHalf + LeftHalf # 注意最后结果左右顺序调换
+         
+    #加密
+    def encrypt(self,input):
+        input = self.hex2bin(input)
+        ip = self.initial_permutations(input)
+        LeftHalf = ip[0:32]
+        RightHalf = ip[32:]
+        output = self.round(LeftHalf,RightHalf,self.subkeys)
+        binary = self.final_permutations(output)
+        return self.bin2hex(binary)
+    
+    # 解密
+    def decrypt(self,input):
+        input = self.hex2bin(input)
+        ip = self.initial_permutations(input)
+        LeftHalf = ip[0:32]
+        RightHalf = ip[32:]
+        output = self.round(LeftHalf,RightHalf,self.subkeys[::-1])
+        binary = self.final_permutations(output)
+        return self.bin2hex(binary)
+    
+   
+    
+
+    
+    
+
+    
+    
+    
 
 # 测试代码
 if __name__ == "__main__":
     
     # 测试数据(明文和密钥都为十六进制表示)
-    plaintext = "11aabbccddeeff01"
+    plaintext = "edeabbccddeeffff"
     key = "cafababedeadbeaf"
     
     des = DES(key)
     
-    
-    # 加密
-    ciphertext = des.encrypt(plaintext)
-    print("加密后的密文:", ciphertext)
-    
-    # 解密
-    decrypted_text = des.decrypt(ciphertext)
-    print("解密后的明文:", decrypted_text)
-    
-    # 测试数据(明文和密钥都为十六进制表示)
-    plaintext = "11aabbccddeeffff"
-    key = "cafababedeadbeaf"
     
     #des = DES(key)
     
